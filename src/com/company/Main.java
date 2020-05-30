17dad66829885d00b835b275f75e5e530b010b62
Jason <50719216+jpeach3@users.noreply.github.com>
2020-05-30 13:43:50 +0100

    hello

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,8 @@
 package com.company;
-        import net.rithms.riot.api.ApiConfig;
-        import net.rithms.riot.api.RiotApi;
-        import net.rithms.riot.api.RiotApiException;
-        import net.rithms.riot.api.endpoints.summoner.dto.Summoner;
-        import net.rithms.riot.constant.Platform;
 
 public class Main {
 
-    public static void main(String[] args) throws RiotApiException {
-        ApiConfig config = new ApiConfig().setKey("RGAPI-57d96f99-0d3a-4d23-b286-9bbd5a20dd0f");
-        RiotApi api = new RiotApi(config);
-
-        Summoner summoner = api.getSummonerByName(Platform.EUW, "Starmental");
-        System.out.println("Name: " + summoner.getName());
-        System.out.println("Summoner ID: " + summoner.getId());
-        System.out.println("Account ID: " + summoner.getAccountId());
-        System.out.println("PUUID: " + summoner.getPuuid());
-        System.out.println("Summoner Level: " + summoner.getSummonerLevel());
-        System.out.println("Profile Icon ID: " + summoner.getProfileIconId());
+    public static void main(String[] args) {
+	// write your code here
     }
 }
-
-
